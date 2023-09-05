fa6ffd9e3bc2600cfad3e5949be21d91e24f4aa7
sourabh verma <sourabhverma3105@gmail.com>
2023-09-05 12:20:30 +0530

    image style changed

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const showImage = document.getElementById("myImage");
 const buttonText = document.getElementById("myButton");
 if(showImage.style.display==="none"){
     showImage.style.display="block";
-    
-    buttonText.innerHTML = "Hide Image"
+    buttonText.innerHTML = "Hide Image";
 }else{
     showImage.style.display="none";
     buttonText.innerHTML = "Show Image"
